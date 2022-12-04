d669e00a31b55947348cc58f15702fc010ac93b4
Karadzhov <amalcho97@gmail.com>
2022-12-04 20:45:19 +0100

    Added ESLint, prettier and Emotion.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { css, Global } from '@emotion/react';
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const global = css`
+  * {
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+export default App;
