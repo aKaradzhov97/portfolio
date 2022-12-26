2816e82d1bef615d2123bdb477520e887e7e87b3
Karadzhov <amalcho97@gmail.com>
2022-12-26 15:57:19 +0100

    Fix contact form submission.

--- src/components/Main/sections/Contact.tsx
+++ src/components/Main/sections/Contact.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { ChangeEvent, FormEvent, memo, useState } from "react";
 import { useMediaQuery } from 'usehooks-ts';
 import { breakpoints } from '@styles';
 import Title from '@components/Title';
@@ -14,11 +14,52 @@ import {
   StyledButton,
 } from './Contact.styled';
 
+const formName = "contact";
+
 type Props = {
   isActive: boolean;
 };
 const ContactSection = ({ isActive }: Props) => {
   const isDesktop = useMediaQuery(`(min-width: ${breakpoints.md})`);
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const encode = (data: Record<string, string>) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': formName,
+        ...formState,
+      }),
+    })
+      .then(() => {
+        form.reset();
+        alert("Thank you! I will get back to you as soon as possible!")
+      })
+      .catch(error => alert(error));
+  };
 
   return (
     <StyledContainer $isActive={isActive}>
@@ -26,24 +67,25 @@ const ContactSection = ({ isActive }: Props) => {
         Contact
       </Title>
       <StyledForm
-        name="contact"
+        name={formName}
         method="post"
+        onSubmit={handleSubmit}
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
         <StyledFields>
-          <input type="hidden" name="contact-form" value="contact" />
+          <input type="hidden" name="contact-form" value={formName} />
           <StyledField $half={isDesktop}>
             <Label htmlFor="name">Name</Label>
-            <Input type="text" name="name" id="name" required={true} />
+            <Input type="text" name="name" id="name" required={true} onChange={handleChange} />
           </StyledField>
           <StyledField $half={isDesktop}>
             <Label htmlFor="email">Email</Label>
-            <Input type="email" name="email" id="email" required={true} />
+            <Input type="email" name="email" id="email" required={true} onChange={handleChange}  />
           </StyledField>
           <StyledField>
             <Label htmlFor="message">Message</Label>
-            <Textarea name="message" id="message" rows={4} required={true} />
+            <Textarea name="message" id="message" rows={4} required={true} onChange={handleChange}  />
           </StyledField>
         </StyledFields>
         <StyledButton type="submit" inverted>
